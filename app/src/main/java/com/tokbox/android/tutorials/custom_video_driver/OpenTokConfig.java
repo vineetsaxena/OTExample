3029d2f71d10644c468c7f9806ad59529fede48a
Vinit Saxena <>
2018-08-30 16:06:26 +0530

    set up

---
+++ app/src/main/java/com/tokbox/android/tutorials/custom_video_driver/OpenTokConfig.java
@@ -0,0 +1,13 @@
+package com.tokbox.android.tutorials.custom_video_driver;
+
+public class OpenTokConfig {
+    // *** Fill the following variables using your own Project info from the OpenTok dashboard  ***
+    // ***                      https://dashboard.tokbox.com/projects                           ***
+
+    // Replace with your OpenTok API key
+    public static final String API_KEY = "45898942";
+    // Replace with a generated Session ID
+    public static final String SESSION_ID = "2_MX40NTg5ODk0Mn5-MTUzNDk0NDExMzE5N35ybVlEekZobUVYQlAxaXNWOGtxSDhJY29-fg";
+    // Replace with a generated token (from the dashboard or using an OpenTok server SDK)
+    public static final String TOKEN = "T1==cGFydG5lcl9pZD00NTg5ODk0MiZzaWc9ODg3ZmZmNDQwNzY3MWNlMjQ5NTQxMjNkZWVlYWVhODU0NTczZGUzYjpzZXNzaW9uX2lkPTJfTVg0ME5UZzVPRGswTW41LU1UVXpORGswTkRFeE16RTVOMzV5YlZsRWVrWm9iVVZZUWxBeGFYTldPR3R4U0RoSlkyOS1mZyZjcmVhdGVfdGltZT0xNTM1MzYzMzY3Jm5vbmNlPTAuNTQwODA5MTQ1OTg1MjkzNSZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNTM3OTU1MzY3JmNvbm5lY3Rpb25fZGF0YT0lN0IlMjJ1c2VySWQlMjIlM0ElMjI1YjU3MDE3NTBmNTY2MTdlMTZhZjU0MTAlMjIlMkMlMjJyb29tJTIyJTNBJTIyc3RyZWFtaW5nLW9wZW4tcm9vbSUyMiUyQyUyMnJvbGUlMjIlM0ElMjJwYXJ0aWNpcGFudCUyMiUyQyUyMnZva2xGb3JtYXQlMjIlM0ElMjJvcGVucm9vbSUyMiUyQyUyMnRpbWVzdGFtcCUyMiUzQTE1MzUzNjMzNjY2NjMlN0Q=";
+}
